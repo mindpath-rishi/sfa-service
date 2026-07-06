4c42d58b9cd1f8984c3f724bfc49aa9460d1d6c7
Lokesh Kose <lokesh.kose@gmail.com>
2026-07-06 12:37:46 +0530

    Updates

--- src/modules/v1/customer/dto/update-customer.dto.ts
+++ src/modules/v1/customer/dto/update-customer.dto.ts
@@ -54,6 +54,11 @@ export class UpdateCustomerDto {
   @ApiPropertyOptional({ type: String, description: 'Reference ID' })
   @IsOptional()
   @IsString()
+  segmentation?: string;
+
+  @ApiPropertyOptional({ type: String, description: 'Reference ID' })
+  @IsOptional()
+  @IsString()
   countryId?: string;
 
   @ApiPropertyOptional({ type: String, description: 'Reference ID' })
