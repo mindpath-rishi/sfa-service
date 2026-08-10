e533f9336c385ba41100a792d2e42a542e21c3d6
Sonu <mihirsejode30@gmail.com>
2026-08-10 19:15:00 +0530

    code cleanup

--- src/modules/v1/customer/customer.module.ts
+++ src/modules/v1/customer/customer.module.ts
@@ -1,34 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-
-// import {
-//   Customer,
-//   CustomerSchema,
-// } from 'src/core/database/mongo/schema/customer.schema';
-// import { RouteCustomerMappingModule } from '../route-customer-mapping/route-customer-mapping.module';
-// import { CustomerController } from './customer.controller';
-// import { CustomerService } from './customer.service';
-// import {
-//   ShopVisit,
-//   ShopVisitSchema,
-// } from 'src/core/database/mongo/schema/shop-visit.schema';
-// import { Sale, SaleSchema } from 'src/core/database/mongo/schema/sale.schema';
-
-// @Module({
-//   imports: [
-//     MongooseModule.forFeature([
-//       { name: Customer.name, schema: CustomerSchema },
-//       { name: ShopVisit.name, schema: ShopVisitSchema },
-//       { name: Sale.name, schema: SaleSchema },
-//     ]),
-//     RouteCustomerMappingModule,
-//   ],
-//   controllers: [CustomerController],
-//   providers: [CustomerService],
-//   exports: [CustomerService],
-// })
-// export class CustomerModule {}
-
 import { Module } from '@nestjs/common';
 
 import { MongooseModule } from '@nestjs/mongoose';
